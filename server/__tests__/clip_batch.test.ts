009fdcfc45894d551111a1d1f096d2be25069596
Jujulu67 <juan.zeiher@viacesi.fr>
2026-03-01 14:20:07 +0100

    test: isolate pglite test database from dev database

--- server/__tests__/clip_batch.test.ts
+++ server/__tests__/clip_batch.test.ts
@@ -76,6 +76,7 @@ function makeClip(
 
 beforeAll(async () => {
 	Bun.env['ENV'] = 'development'
+	Bun.env['DEV_DATABASE_FOLDER'] = 'dev.database.test'
 
 	const databaseMod = await import('../database')
 	const historyMod = await import('../history')
